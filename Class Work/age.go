5c40358b3954e88318c2bb172c13cd62a5de312f
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-26 21:48:23 -0800

    fix: age calculator

--- Class Work/age.go
+++ Class Work/age.go
@@ -5,18 +5,29 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Enter your date of birth: ")
-	stringDob, _ := reader.ReadString('\n')
+	fmt.Print("Enter your month of birth (as a number): ")
+	stringMonth, _ := reader.ReadString('\n')
+	fmt.Print("Enter your day of birth: ")
+	stringDay, _ := reader.ReadString('\n')
 	fmt.Print("Enter your age: ")
 	stringAge, _ := reader.ReadString('\n')
 
-	dob, _ := strconv.Atoi(stringDob[:len(stringDob)-1])
+	month, _ := strconv.Atoi(stringMonth[:len(stringMonth)-1])
+	day, _ := strconv.Atoi(stringDay[:len(stringDay)-1])
 	age, _ := strconv.Atoi(stringAge[:len(stringAge)-1])
-	currentYear := dob + age
-	fmt.Println("You are currently living in: ", currentYear)
+
+	now := time.Now()
+
+	if month > int(now.Month()) || (month == int(now.Month()) && day >= now.Day()) {
+		fmt.Println("hello")
+		fmt.Println("You were born:", now.Year()-age-1)
+	} else {
+		fmt.Println("You were born:", now.Year()-age)
+	}
 }
