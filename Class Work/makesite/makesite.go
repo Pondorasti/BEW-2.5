a31359a7721c12b7e87b0a6de213d2ffae8d239d
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-01-28 11:39:17 -0800

    feat: render template

--- Class Work/makesite/makesite.go
+++ Class Work/makesite/makesite.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
-func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+type Article struct {
+	Title      string
+	Paragraphs []string
+}
 
+func main() {
+	fileContents, err := ioutil.ReadFile("data/first-post.txt")
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Print(string(fileContents))
+	article := Article{}
+	for index, line := range strings.Split(string(fileContents), "\n") {
+		if index == 0 {
+			article.Title = line
+		} else if line != "" {
+			article.Paragraphs = append(article.Paragraphs, line)
+		}
+	}
+
+	templateContents, err := ioutil.ReadFile("template.tmpl")
+	if err != nil {
+		panic(err)
+	}
+
+	t := template.Must(template.New("template.tmpl").Parse(string(templateContents)))
+	err = t.Execute(os.Stdout, article)
+	if err != nil {
+		fmt.Println("hello")
+		panic(err)
+	}
 }
