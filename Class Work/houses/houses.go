de7b05318e1f762e2236efbfeb2760ec0d96d477
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2022-02-02 21:10:39 -0800

    feat: houses

--- Class Work/houses/houses.go
+++ Class Work/houses/houses.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/alexflint/go-arg"
 )
 
 type House struct {
-	// numberOfRooms string
-	City string
-	// address       string
-	Price string
+	NumberOfRooms []int    `arg:"-n,separate"`
+	City          []string `arg:"-c,separate"`
+	Address       []string `arg:"-a,separate"`
+	Price         []int    `arg:"-p,separate"`
 }
 
-func main() {
-	fmt.Println("Hello world")
+// Usage Example
+// go run houses.go -c "San Francisco" -a "555 Post" -p 123456 -n 8 -c "Lakeover" -a "Structural Lane Nr. 38th" -p 90000 -n 4
 
+func main() {
 	var args House
 	arg.MustParse(&args)
-	fmt.Println(args.City, args.Price)
+
+	w := tabwriter.NewWriter(os.Stdout, 2, 2, 2, ' ', 0)
+	fmt.Fprintln(w, "Address\tCity\tRooms\tPrice")
+	for i := 0; i < len(args.Address); i++ {
+		fmt.Fprintf(w, "%s\t%s\t%d\t$%d\n", args.Address[i], args.City[i], args.NumberOfRooms[i], args.Price[i])
+	}
+	w.Flush()
 }
